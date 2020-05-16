1c0223d7a6222912f91b9bfe9b25e9405d8f7c33
Risha Ramanathan <risha_ramanathan@yahoo.com>
2020-05-16 14:02:35 +0800

    refactor: Updated function name for clarity
    Changed function name data to data_viewing_option for more clarity its 
    purpose.

--- bikeshare.py
+++ bikeshare.py
@@ -214,7 +214,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def data(df):
+def data_viewing_option(df):
     """Prompt user if they would like to see lines of raw data"""
 
     raw_data = 0
@@ -246,7 +246,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        data(df)
+        data_viewing_option(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
